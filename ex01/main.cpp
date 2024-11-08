70016cde2950ea1d6233bd733a6cf4c4e6632044
Eleni Perperidou <eperperi@1-E-1.42heilbronn.de>
2024-11-08 17:08:34 +0100

    main ready

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eperperi <eperperi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 15:01:41 by eperperi          #+#    #+#             */
-/*   Updated: 2024/11/08 13:21:02 by eperperi         ###   ########.fr       */
+/*   Updated: 2024/11/08 13:21:38 by eperperi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 int main(void)
 {
-
+	int num = 3;
+	Zombie *horde;
+	
+	horde = zombieHorde(num, "Foo");
+	for (int i = 0; i < num; i++)
+	{
+		std::cout << "Index" << i << ": ";
+		horde[i].announce();
+	}
+	delete[] horde;
+	return 0;
 }
